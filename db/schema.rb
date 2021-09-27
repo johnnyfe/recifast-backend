bf987e4ae66939a5e8fc37f3421cc954df06a4a5
johnny <johnny1194@hotmail.com>
2021-09-27 13:51:16 -0400

    migrating comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_174203) do
+ActiveRecord::Schema.define(version: 2021_09_27_174950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "descriptionrating"
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.integer "preparation_time"
@@ -37,5 +47,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_174203) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "recipes"
+  add_foreign_key "comments", "users"
   add_foreign_key "recipes", "users"
 end
