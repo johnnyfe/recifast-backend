75d965a2d0c0c523c6bad1a807d695489d720e34
johnny <johnny1194@hotmail.com>
2021-09-29 13:02:37 -0400

    adding relationship models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+    has_many :cooking_lists, dependent: :destroy
+    has_many :ingredients, through: :cooking_lists
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :ingredients, through: :cooking_lists
 end
