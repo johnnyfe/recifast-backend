fdbd92b95a00f49bfc61d3e3733c259ef4e1f357
johnny <johnny1194@hotmail.com>
2021-09-29 13:48:27 -0400

    adding column_name name to recipe to scheema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_181613) do
+ActiveRecord::Schema.define(version: 2021_09_29_174356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_181613) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
