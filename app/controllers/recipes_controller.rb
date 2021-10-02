ea7e31469348cffc3cec24463d808794d705e322
johnny <johnny1194@hotmail.com>
2021-10-02 09:38:50 -0400

    adding record not found and invalid methods to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     
     def index
         recipes = Recipe.all
@@ -20,5 +23,14 @@ class RecipesController < ApplicationController
     def params_recipe
         params.permit(:calories, :preparation_time, :video_url, :instructions, :description, :user, :name)
     end
+
+    def render_not_found
+        render json: { error: "recipe not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
     
 end
