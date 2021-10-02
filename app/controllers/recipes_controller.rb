cf5e3730092f00bb77fd84d2083a72a5b3bdf237
johnny <johnny1194@hotmail.com>
2021-10-02 09:33:38 -0400

    adding create method to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,15 @@ class RecipesController < ApplicationController
         render json: recipe
     end
 
-    
+    def create
+        recipe = Recipe.create!(params_recipe)
+        render json: recipe, status: :created
+    end
+
+    private
+
+    def params_recipe
+        params.permit(:calories, :preparation_time, :video_url, :instructions, :description, :user, :name)
+    end
     
 end
