8a2468543a6f97e32d0c0eb0cd7a5810fe523f17
johnny <johnny1194@hotmail.com>
2021-10-03 09:55:13 -0400

    adding cooking list serializer

--- app/controllers/cooking_lists_controller.rb
+++ app/controllers/cooking_lists_controller.rb
@@ -1,2 +1,51 @@
 class CookingListsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
+    def index
+        cookingLists = CookingList.all
+        render json: cookingLists
+    end
+
+    def show
+        cookingList = find_cookingList
+        render json: cookingList
+    end
+
+    def create
+        cookingList = CookingList.create!(params_cookingList)
+        render json: cookingList, status: :created
+    end
+
+    def update
+        cookingList = find_cookingList
+        cookingList.update!(params_cookingList)
+        render json: cookingList, status: :accepted
+    end
+
+    def destroy
+        cookingList = find_cookingList
+        cookingList.destroy
+        head :no_content
+    end
+
+    private
+
+    def params_cookingList
+        params.permit(:name, :calories, :category, :image_url, :price, :quantity, :portion)
+    end
+    
+    def find_cookingList
+        CookingList.find(params[:id])
+    end
+
+    def render_not_found
+        render json: { error: "cookingList not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
