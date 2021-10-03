a0bed6893519d7262cd68956a42b0a390b10fd39
johnny <johnny1194@hotmail.com>
2021-10-03 09:27:56 -0400

    adding validations for ingredient models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient < ApplicationRecord
     has_many :cooking_lists, dependent: :destroy
     has_many :ingredients, through: :cooking_lists
+
+    validates :name, uniqueness: true, presence: true
+    validates :calories, presence: true, numericality: { only_integer: true, less_than: 1001}
+    validates :image_url, presence: true, uniqueness: true 
+    validates :category, presence: true
+    validates :price, presence: true, numericality: { only_integer: true, more_than:1, less_than: 10001}
+    validates :quantity, presence: true, numericality: { only_integer: true, more_than:1, less_than: 101}
+    validates :portion, presence: true, numericality: { only_integer: true, more_than:1, less_than: 1001}
 end
