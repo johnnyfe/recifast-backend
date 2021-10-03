5de5c7e1b233b85a48045f7ac3814c1a37be12d9
johnny <johnny1194@hotmail.com>
2021-10-03 09:49:14 -0400

    fixing error in serializer recipe

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes id:, :calories, :preparation_time, :image_url, :video_url, :instructions, :description, :name
+  attributes :id, :calories, :preparation_time, :image_url, :video_url, :instructions, :description, :name
   has_one :user, serializer: RecipeUserSerializer
 end
