b96474c7227b3c75081c364d25ba5bcd41e9e089
johnny <johnny1194@hotmail.com>
2021-10-03 14:05:45 -0400

    adding secured password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :recipes, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, length: { in: 6..20 }
 end
