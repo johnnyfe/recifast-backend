3dca1c788b0e2c3989fa81bb8ae00c298b088db4
johnny <johnny1194@hotmail.com>
2021-10-03 10:28:33 -0400

    adding validation for cooking list model

--- app/models/cooking_list.rb
+++ app/models/cooking_list.rb
@@ -1,4 +1,7 @@
 class CookingList < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :ingredient_quantity, presence: true, numericality: { only_integer: true, more_than:1, less_than: 101}
+  validates :ingredient_portion, presence: true, numericality: { only_integer: true, more_than:1, less_than: 1001}
 end
