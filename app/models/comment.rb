78e9e076c6f0cf9c721a9bdcdad12b928e40c8e8
johnny <johnny1194@hotmail.com>
2021-10-03 20:25:32 -0400

    adding validation for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, less_than: 6}
 end
