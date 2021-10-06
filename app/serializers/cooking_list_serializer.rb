9c16e5d49ec1b4d724c1101a6ecc8187365624d4
johnny <johnny1194@hotmail.com>
2021-10-06 16:16:32 -0400

    removing ingredient portion and quantity from serializer

--- app/serializers/cooking_list_serializer.rb
+++ app/serializers/cooking_list_serializer.rb
@@ -1,3 +1,3 @@
 class CookingListSerializer < ActiveModel::Serializer
-  attributes :id, :recipe_id, :ingredient_id, :ingredient_portion, :ingredient_quantity
+  attributes :id, :recipe_id, :ingredient_id
 end
