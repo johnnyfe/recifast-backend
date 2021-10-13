584dc224b52181848f88577157815341be020bed
johnny <johnny1194@hotmail.com>
2021-10-13 13:51:18 -0400

    removing unnecessary migration for cooking list

--- db/migrate/20211006171020_remove_ingredient_portion_and_quantity.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIngredientPortionAndQuantity < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :cooking_lists, :ingredient_portion, :integer
-    remove_column :cooking_lists, :ingredient_quantity, :integer
-  end
-end
